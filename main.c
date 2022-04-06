7f63f3f1d6725a57d60bc70eddb0c4d12baab995
Roman1 <68794830+0xroman1@users.noreply.github.com>
2022-04-05 20:40:56 -0500

    Update and rename main.c to target.c

--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	printf("I am the target\n");
-    	return 0;
-}
-
---
+++ target.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// JUST A DUMMY EXECUTABLE TO INFECT
+
+int main(){
+	printf("I am the target. Infect me.\n");
+    	return 0;
+}
+
